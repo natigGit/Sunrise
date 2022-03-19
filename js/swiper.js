54c0cdb62dc8c5fde6c842593ee9360f508e3c62
LekenSuvar <87902339+LekenSuvar@users.noreply.github.com>
2022-03-20 00:41:12 +0400

    Sunrise adaptive from 1450 to 767

--- js/swiper.js
+++ js/swiper.js
@@ -4,11 +4,27 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 2.3,
     spaceBetween: 100,
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+    breakpoints: {
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 1.1,
+        spaceBetween: 20,
+      },
+      760: {
+        slidesPerView: 1.2,
+        spaceBetween: 50,
+      },
+      991:{
+        slidesPerView: 1.8,
+        spaceBetween: 100,
+      },
+      1144: {
+        slidesPerView: 2.3
+      },
+    }
   });
