4df00e60893f3ff0f1572af884e0c1a8acd534ef
LekenSuvar <87902339+LekenSuvar@users.noreply.github.com>
2022-07-17 16:00:22 +0400

    Add files via upload

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,7 @@
 const list = document.querySelector('.list'),
       items = document.querySelectorAll('.blocks__item')
       listItems = document.querySelectorAll('.list__item')
+      block = document.querySelectorAll('.blocks__row')
 
 function filter() {
     list.addEventListener('click', event => {
@@ -16,18 +17,33 @@ function filter() {
         switch(targetId) {
             case 'all':
                 getItems('blocks__item')
+                block.forEach(item => {
+                    item.style.justifyContent = 'space-evenly'
+                })
                 break
             case 'design':
                 getItems(targetId)
+                block.forEach(item => {
+                    if(window.innerWidth > 800)item.style.justifyContent = 'start'
+                })
                 break
             case 'front':
                 getItems(targetId)
+                block.forEach(item => {
+                    if(window.innerWidth > 800)item.style.justifyContent = 'start'
+                })
                 break
             case 'android':
                 getItems(targetId)
+                block.forEach(item => {
+                    if(window.innerWidth > 800)item.style.justifyContent = 'start'
+                })
                 break
             case 'back':
                 getItems(targetId)
+                block.forEach(item => {
+                    if(window.innerWidth > 800)item.style.justifyContent = 'start'
+                })
                 break
         }
     })
